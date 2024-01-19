eb2f57a50fcb475bca9f72b82bac98519397c518
tess61 <hopemelkamu28@gmail.com>
2024-01-19 11:47:05 +0300

    fix issue of GetAllEmployee & pom.xml configured

--- src/main/java/net/javaguides/ems/dto/EmployeeDto.java
+++ src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -14,5 +14,4 @@ public class EmployeeDto {
 	private String firstName;
 	private String lastName;
 	private String email;
-	
 }
--- src/main/java/net/javaguides/ems/entity/Employee.java
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -28,4 +28,6 @@ public class Employee {
 	private String lastName;
 	@Column(name="email_id", nullable=false, unique= true)
 	private String email;
+	
+	
 }
